63bb5431d0a4e9e8f1624daf26abeb89ade613b9
Hawre <131952165+0xHawre@users.noreply.github.com>
2024-09-11 22:54:25 +0330

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,58 +1,65 @@
 #!/bin/bash
 
-# Function to check if Docker is installed and install it if not
-check_and_install_docker() {
-    if ! command -v docker &> /dev/null; then
-        echo "Docker is not installed. Installing Docker..."
-        
-        # Update package list and install prerequisites
-        sudo apt-get update
-        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt update && sudo apt install -y curl && curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
 
-        # Add Docker's official GPG key
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-        # Add Docker's stable repository
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install npm -y
 
-        # Update package list and install Docker
-        sudo apt-get update
-        sudo apt-get install -y docker-ce
+sudo npm i n -g
 
-        echo "Docker has been installed."
-    else
-        echo "Docker is already installed."
-    fi
+sudo n stable
+
+sudo npm i -g yarn
+
+git clone https://github.com/CATProtocol/cat-token-box && cd cat-token-box
+sudo chown -R $USER:$USER ~/cat-token-box
+
+sudo yarn install && yarn build
+
+cd packages/tracker
+
+sudo chmod 777 docker/data && sudo chmod 777 docker/pgdata && sudo docker compose up -d
+
+cd $HOME/cat-token-box && sudo docker build -t tracker:latest .
+
+read -p "node name : vname
+
+sudo docker run -d \
+  --name $(vname) \
+    --add-host="host.docker.internal:host-gateway" \
+    -e DATABASE_HOST="host.docker.internal" \
+    -e RPC_HOST="host.docker.internal" \
+    -p 3000:3000 \
+    tracker:latest
+
+cd $HOME/cat-token-box/packages/cli
+
+cat <<EOF > config.json
+{
+  "network": "fractal-mainnet",
+  "tracker": "http://127.0.0.1:3000",
+  "dataDir": ".",
+  "maxFeeRate": 1000,
+  "rpc": {
+      "url": "http://127.0.0.1:8332",
+      "username": "bitcoin",
+      "password": "opcatAwesome"
+  }
 }
-
-# Call the function to check and install Docker
-check_and_install_docker
-
-# Prompt the user for input
-read -p "Enter STRATEGY_EXECUTOR_IP_ADDRESS: " ip_address
-read -p "Enter STRATEGY_EXECUTOR_DISPLAY_NAME: " validator_name
-read -p "Enter STRATEGY_EXECUTOR_BENEFICIARY: " safe_public_address
-read -p "Enter SIGNER_PRIVATE_KEY: " private_key
-
-# Replace placeholders in the environment variables and save to validator.env
-cat <<EOF > validator.env
-ENV=testnet-3
-STRATEGY_EXECUTOR_IP_ADDRESS=${ip_address}
-STRATEGY_EXECUTOR_DISPLAY_NAME=${validator_name}
-STRATEGY_EXECUTOR_BENEFICIARY=${safe_public_address}
-SIGNER_PRIVATE_KEY=${private_key}
 EOF
 
-echo "Environment variables have been set and saved to validator.env file."
+sudo yarn cli wallet create
+
+sudo yarn cli wallet address
 
 
-# Pull the Docker image
-docker pull elixirprotocol/validator:v3
+echo "Do you want to see your seed? (yes/no)"
+read answer
 
-# Run the Docker container with the environment file
-docker run -it \
-  --env-file validator.env \
-  --name elixir \
-  elixirprotocol/validator:v3
-
-
+if [ "$answer" = "yes" ]; then
+    cat wallet.json
+else
+    echo "Continuing without showing the seed."
+fi
